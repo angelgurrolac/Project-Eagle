0a704aa4d878b9f3708ea315e6b3cfb66b3bb535
angelgurrola <angel_gurrolac@outlook.com>
2015-08-06 00:28:01 -0500

    ajax
    Correcciones

---
+++ project/layout/php/conexion.php
@@ -0,0 +1,28 @@
+<?php
+ 
+function Conectarse() 
+{ 
+   if (!($link=mysql_connect("localhost","root","admin"))) 
+   { 
+      //echo "Error conectando a la base de datos."; 
+      exit(); 
+   } 
+   if (!mysql_select_db("projecte",$link)) 
+   { 
+      //echo "Error seleccionando la base de datos."; 
+      exit(); 
+   } 
+
+   return $link; 
+
+
+
+} 
+
+$link=Conectarse();
+mysql_query("SET NAMES 'utf8'");
+
+
+
+
+?> 
