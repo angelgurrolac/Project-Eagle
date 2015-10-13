1dcc7b44169f8512a45f3031ab1500a99438b2ce
Zayra Chavez <zayrachavez@hotmail.com>
2015-10-13 00:26:22 -0500

    Front-end Inicio de sesión terminado
    Corregí el nombre de la carpeta porque se lo había puesto mal, e hice de
    nuevo el front de inicio de sesión con bootstrap

---
+++ projecte/assets/js/funciones.js
@@ -0,0 +1,11 @@
+<SCRIPT  language=JavaScript> 
+function go(){
+
+if (document.form.contrasena.value=='contrasena && document.form.login.value=='usuario'){ 
+        document.form.submit(); 
+    } 
+    else{ 
+         alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+    } 
+} 
+</SCRIPT> 
